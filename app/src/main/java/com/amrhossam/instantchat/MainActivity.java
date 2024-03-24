44a31836dbd5533baf141d0c3db6f8fb4bd72f86
Amr Hossam <amrhossam.dev@gmail.com>
2024-03-24 02:51:36 +0200

    Supported Android 14

--- app/src/main/java/com/amrhossam/instantchat/MainActivity.java
+++ app/src/main/java/com/amrhossam/instantchat/MainActivity.java
@@ -1,31 +1,28 @@
 package com.amrhossam.instantchat;
 
 import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
-import android.telephony.PhoneNumberUtils;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import com.amrhossam.instantchatm.R;
 import com.hbb20.CountryCodePicker;
 
 
 public class MainActivity extends AppCompatActivity {
     CountryCodePicker ccp;
 
-
-    //Donate Me https://www.paypal.me/amrhossamdev
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,56 +33,51 @@ public class MainActivity extends AppCompatActivity {
         final LinearLayout liner = findViewById(R.id.linear);
         ccp = findViewById(R.id.ccp);
 
-        //Donate Me https://www.paypal.me/amrhossamdev
 
-
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                final String Code = ccp.getSelectedCountryCode();
-                if (editText.getText().toString().trim().length() <= 0) {
-
-                    Snackbar snackbar = Snackbar
-                            .make(liner, getResources().getString(R.string.sorry), Snackbar.LENGTH_LONG);
-                    snackbar.show();
-                } else {
-
-
-                    String smsNumber = editText.getText().toString();
-
-                    boolean isWhatsappInstalled = whatsappInstalledOrNot("com.whatsapp");
-                    if (isWhatsappInstalled) {
-                        Intent sendIntent = new Intent("android.intent.action.MAIN");
-                        sendIntent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Conversation"));
-                        sendIntent.putExtra("jid", Code + PhoneNumberUtils.stripSeparators(smsNumber) + "@s.whatsapp.net");
-
-
-                        startActivity(sendIntent);
-                    } else {
-
-                        Snackbar snackbar = Snackbar
-                                .make(liner, "WhatsApp not Installed", Snackbar.LENGTH_LONG);
-
-                        snackbar.show();
+        button.setOnClickListener(v -> {
+            final String Code = ccp.getSelectedCountryCode();
+            if (editText.getText().toString().trim().length() == 0) {
+                Snackbar snackbar = Snackbar.make(liner, getResources().getString(R.string.sorry), Snackbar.LENGTH_LONG);
+                snackbar.show();
+            } else {
+                String smsNumber = editText.getText().toString();
+                boolean isWhatsappInstalled = isWhatsAppInstalled(this);
+                if (isWhatsappInstalled) {
+                    try {
+                        openChat(Code + smsNumber);
+                    } catch (Exception e) {
+                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
-
+                } else {
+                    Snackbar snackbar = Snackbar.make(liner, "WhatsApp not Installed", Snackbar.LENGTH_LONG);
+                    snackbar.show();
                 }
+
             }
         });
     }
 
-    private boolean whatsappInstalledOrNot(String uri) {
-        PackageManager pm = getPackageManager();
-        boolean app_installed = false;
-        try {
-            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
-            app_installed = true;
-        } catch (PackageManager.NameNotFoundException e) {
-            app_installed = false;
-        }
-        return app_installed;
+    private void openChat(String num) {
+        String smsNumber = num + "@s.whatsapp.net";
+        Uri uri = Uri.parse("smsto:" + smsNumber);
+        Intent i = new Intent(Intent.ACTION_SENDTO, uri);
+        i.putExtra("jid", smsNumber);
+        i.setPackage("com.whatsapp");
+        this.startActivity(i);
     }
+
+    public static boolean isWhatsAppInstalled(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        boolean isInstalled;
+        try {
+            packageManager.getPackageInfo("com.whatsapp", PackageManager.GET_ACTIVITIES);
+            isInstalled = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            isInstalled = false;
+        }
+        return isInstalled;
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -96,38 +88,33 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.rate:
-                Uri uri = Uri.parse("market://details?id=" + MainActivity.this.getPackageName());
-                Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
-                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
-                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
-                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
-                try {
-                    startActivity(goToMarket);
-                } catch (ActivityNotFoundException e) {
-                    startActivity(new Intent(Intent.ACTION_VIEW,
-                            Uri.parse("http://play.google.com/store/apps/details?id=" + MainActivity.this.getPackageName())));
-                }
-
-                break;
-            case R.id.share:
-                Intent sendIntent = new Intent();
-                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-                sharingIntent.setType("text/plain");
-                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getResources().getString(R.string.app_name));
-                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, getResources().getString(R.string.info) + "\n" + " https://play.google.com/store/apps/details?id=com.amrhossam.instantchat");
-                startActivity(Intent.createChooser(sharingIntent, "Share app via"));
-
-                break;
+        int itemId = item.getItemId();
+        if (itemId == R.id.rate) {
+            Uri uri = Uri.parse("market://details?id=" + MainActivity.this.getPackageName());
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_NEW_DOCUMENT | Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+            }
+            try {
+                startActivity(goToMarket);
+            } catch (ActivityNotFoundException e) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + MainActivity.this.getPackageName())));
+            }
+        } else if (itemId == R.id.share) {
+            Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+            sharingIntent.setType("text/plain");
+            sharingIntent.putExtra(Intent.EXTRA_SUBJECT, getResources().getString(R.string.app_name));
+            sharingIntent.putExtra(Intent.EXTRA_TEXT, getResources().getString(R.string.info) + "\n" + " https://play.google.com/store/apps/details?id=com.example.app"); // Adjusted for clarity
+            startActivity(Intent.createChooser(sharingIntent, "Share app via"));
+        } else {
+            return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
+        return true;
+
     }
 
     public void onBackPressed() {
         super.onBackPressed();
         finish();
-
-
     }
 }
